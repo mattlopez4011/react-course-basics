b3658a2927bf7523332636e656b606b57a207d43
Matt Lopez <matthew.lopez4011@gmail.com>
2020-04-05 13:13:12 -0500

    start building class based Counter component

---
+++ src/components/counter.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    // imageUrl: 'https://picsum.photos/200'
+    tags: ["tags", "tags2", "tags3"],
+  };
+
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+git
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement() {
+    console.log("Increment Clicked", this);
+  }
+
+  render() {
+    // let classes = this.getBadgeClasses();
+
+    return (
+      <div>
+        {/*<img src={this.state.imageUrl} alt=""/>*/}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className={"btn btn-secondary btn-sm"}
+        >
+          Increment
+        </button>
+        <div>
+          {this.state.tags.length === 0 && "Please create a new tag!"}
+          {this.renderTags()}
+        </div>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
